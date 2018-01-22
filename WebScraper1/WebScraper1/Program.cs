8da074c18da7a56444b48abfda96534b096999d4
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-01-21 22:22:46 -0500

    learning to access url for article link using xpath

--- WebScraper1/WebScraper1/Program.cs
+++ WebScraper1/WebScraper1/Program.cs
@@ -7,19 +7,17 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Data.Entity;
 
-namespace WebScraper1
+namespace WebScraper_Classes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var options = new ChromeOptions();
-            //options.AddArguments("--disable-gpu");
-
-            // open chrome and provide destination address
-            var chromeDriver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            ChromeDriver chromeDriver = new ChromeDriver();
             chromeDriver.Navigate().GoToUrl("https://www.progressivegrocer.com");
 
+            //var article = new ArticleDB() {ArticleTitle=" };
 
             // Find article titles
             var titles = chromeDriver.FindElementsByClassName("title");
@@ -39,9 +37,18 @@ namespace WebScraper1
                 Console.WriteLine(title.Text);
             }
 
+            //var urlFinder = "//*[@id='block-homepageprimaryarticles']/div/div[1]/div/div[2]/a";
+
+            //foreach (var url in urlFinder)
+            //{
+            //    Console.WriteLine(url.ToString());
+            //}
+
             chromeDriver.Quit();
 
             ArticleDB DB1 = new ArticleDB();
+            DB1.Source = "Progressive Grocer";
+            DB1.SourceUrl = "https://www.progressivegrocer.com";
         }
 
         public class ArticleDB
@@ -51,20 +58,33 @@ namespace WebScraper1
 
             }
 
-            public int ArticleID { get; set; }
+            //public int ArticleID { get; set; }
             public string ArticleTitle { get; set; }
             public string Source { get; set; }
-            public string Url { get; set; }
+            public string SourceUrl { get; set; }
         }
 
-        //public class WebScraper
+        public class ArticleDBContext : DbContext
+        {
+            public ArticleDBContext() : base()
+            {
+
+            }
+
+            public DbSet<ArticleDB> ArticlesDB { get; set; }
+        }
+
+        //public class Navigation
         //{
-        //    public WebScraper()
+        //    public Navigation()
         //    {
 
         //    }
+        //}
 
-        //    public void ScrapeMethod()
+        //public class Article_Finder
+        //{
+        //    public Article_Finder()
         //    {
 
         //    }
